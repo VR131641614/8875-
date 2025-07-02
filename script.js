bed1af9eb237a0a4c11a0e67a6f025d2248ca343
VR131641614 <salemfa741614@gmail.com>
2025-07-02 19:26:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('section#tools ul li').forEach(function (item) {
-        item.addEventListener('click', function () {
-            alert('تنبيه: استخدم هذه الأدوات فقط بشكل قانوني!');
-        });
-    });
-});
